839bcf87f1eb60c71867449b356bf6c13142b713
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-08 07:47:12 +0530

    correction added in to controllers

---
+++ src/controllers/ClientFormController.java
@@ -0,0 +1,19 @@
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClientFormController {
+    public TextArea textArea;
+    public TextField textMessage;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+}
