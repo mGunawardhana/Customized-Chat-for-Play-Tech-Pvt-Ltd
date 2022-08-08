c1d2365e0f16451fa44fff2e6c9c0a61588dff62
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-08 07:39:00 +0530

    initial state of Play Tech Pvt Ltd

---
+++ src/ClientInitializer.java
@@ -0,0 +1,25 @@
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("ui/ClientForm.fxml"))));
+        primaryStage.show();
+    }
+}
