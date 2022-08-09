604c14d7982aefadf996007d9441187d5471068e
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-09 20:14:30 +0530

    implement the client form three

--- src/controllers/ClientFormController3.java
+++ src/controllers/ClientFormController3.java
@@ -1,3 +1,9 @@
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
 package controllers;
 
 import javafx.event.ActionEvent;
@@ -20,12 +26,28 @@ public class ClientFormController3 {
     String message3 = "";
 
     public void initialize() {
+        new Thread(() -> {
 
+            try {
+                socket3 = new Socket("localhost", port3);
+
+                dataInputStream3 = new DataInputStream(socket3.getInputStream());
+                dataOutputStream3 = new DataOutputStream(socket3.getOutputStream());
+
+                while (!message3.equals("exit")) {
+                    message3 = dataInputStream3.readUTF();
+                    textArea3.appendText(message3+"\n");
+                }
+
+            } catch (IOException ignored) {
+            }
+        }).start();
     }
 
 
     public void sendOnAction3(ActionEvent actionEvent) throws IOException {
-
+        dataOutputStream3.writeUTF(textMessage3.getText().trim());
+        dataOutputStream3.flush();
     }
 
 }
