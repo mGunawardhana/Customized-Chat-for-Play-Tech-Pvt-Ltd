2b957cabd44b8f8a609a61638b38fbf627004950
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-09 14:02:43 +0530

    implementing Client form one to connect the server using port no 5000

--- src/controllers/ClientFormController.java
+++ src/controllers/ClientFormController.java
@@ -10,10 +10,48 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientFormController {
+    final int PORT = 5000;
+    Socket accept;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String message = "";
+
     public TextArea textArea;
     public TextField textMessage;
 
-    public void sendOnAction(ActionEvent actionEvent) {
+    public void initialize() {
+
+        new Thread(() -> {
+            try {
+
+                accept = new Socket("localhost", PORT);
+
+                dataInputStream = new DataInputStream(accept.getInputStream());
+                dataOutputStream = new DataOutputStream(accept.getOutputStream());
+
+                while (!message.equals("exit")) {
+                    message = dataInputStream.readUTF();
+                    textArea.appendText(message + "\n");
+                }
+
+                dataOutputStream.writeUTF(message.trim());
+                dataOutputStream.flush();
+
+            } catch (IOException ignored) {
+            }
+
+        }).start();
+
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        dataOutputStream.writeUTF(textMessage.getText().trim());
+        dataOutputStream.flush();
     }
 }
