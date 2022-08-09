6f4a4d70132f7e6e8db447c48ef8b34db82026f1
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-09 14:16:50 +0530

    completing the chat with server and client two

--- src/controllers/ClientFormController2.java
+++ src/controllers/ClientFormController2.java
@@ -10,10 +10,44 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientFormController2 {
     public TextArea textArea2;
     public TextField textMessage2;
 
-    public void sendOnAction2(ActionEvent actionEvent) {
+    final int PORT = 8000;
+    Socket socket2;
+    DataInputStream dataInputStream2;
+    DataOutputStream dataOutputStream2;
+    String message2 = "";
+
+    public void initialize() {
+        new Thread(() -> {
+
+            try {
+                socket2 = new Socket("localhost", PORT);
+
+
+                dataOutputStream2 = new DataOutputStream(socket2.getOutputStream());
+                dataInputStream2 = new DataInputStream(socket2.getInputStream());
+
+                while (!message2.equals("exit")) {
+                    message2 = dataInputStream2.readUTF();
+                    textArea2.appendText(message2 + "\n");
+
+                }
+
+            } catch (IOException ignored) {
+            }
+
+        }).start();
+    }
+    public void sendOnAction2(ActionEvent actionEvent) throws IOException {
+        dataOutputStream2.writeUTF(textMessage2.getText().trim());
+        dataOutputStream2.flush();
     }
 }
