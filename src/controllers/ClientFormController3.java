e892545bcacda9c63698e4b0de5912377087e5ba
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-09 13:51:35 +0530

    Implementing structure of Client 3

---
+++ src/controllers/ClientFormController3.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientFormController3 {
+
+    public TextArea textArea3;
+    public TextField textMessage3;
+
+    public void initialize() {
+    }
+
+
+    public void sendOnAction3(ActionEvent actionEvent) throws IOException {
+
+    }
+
+}
