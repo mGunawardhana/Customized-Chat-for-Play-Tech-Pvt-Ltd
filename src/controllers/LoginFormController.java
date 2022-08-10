6a76f745897336e5f61beeae1e0f17bc25d08e3b
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-10 07:44:29 +0530

    adding logic to check correct users

--- src/controllers/LoginFormController.java
+++ src/controllers/LoginFormController.java
@@ -16,5 +16,27 @@ public class LoginFormController {
     public PasswordField passwordField;
 
     public void loginOnAction(ActionEvent actionEvent) {
+
+        if (userNameTxt.getText().trim().equals("maneesha")) {
+            if (passwordField.getText().trim().equals("12345")) {
+                //TODO : load here in client one
+            } else {
+                System.out.println("Incorrect try!");
+            }
+        } else if (userNameTxt.getText().trim().equals("sasmitha")) {
+            if (passwordField.getText().trim().equals("12345")) {
+                //TODO : load here in client two
+            } else {
+                System.out.println("Incorrect try!");
+            }
+        } else if (userNameTxt.getText().trim().equals("null")) {
+            if (passwordField.getText().trim().equals("12345")) {
+                //TODO : load here in client three
+            }
+        } else {
+            System.exit(0);
+        }
+
     }
 }
+
